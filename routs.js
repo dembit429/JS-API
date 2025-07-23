c59262441d1da0ae0b00cd3d6e173cf0b67e2922
Danylo Dembitskyi <112881771+dembit429@users.noreply.github.com>
2025-07-23 20:36:50 +0300

    Dev (#2)
    * JWT integration
    
    * JWT fixed
    
    * User adding fixed
    
    * Refresh token added in cookies
    
    * Added a nodemon for refreshing server
    
    * Added a message when establishing a connection to a db
    
    * Added an env example
    
    * Testing hook
    
    * Adding /users to router,and testing userService class
    
    * Scalled user service,wrong json req causes crash(needs to be fixed)
    
    * Delete method added to service.
    
    * Deleted commented code and unused imports
    
    * Fixed typo errors,according to review

--- routs.js
+++ routs.js
@@ -1,81 +1,138 @@
 import express from "express";
 import pool from "./db.js";
+import jwt from 'jsonwebtoken';
+import { generateAccessToken, authenticateAccessToken,generateRefreshToken } from "./middleware/authetification.js";
+import bcrypt from "bcrypt";
+import UserService from "./services/user.js";
 
+const userService = new UserService();
 const userRouter = express.Router();
 
-userRouter.get("/users",async (req,res)=>{
+userRouter.post('/refresh', (req, res) => {
+  const token = req.cookies.refreshToken;
+  console.log("Received refresh token:", token);
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      console.error("Refresh token verification failed:", err);
+      return res.sendStatus(403);
+    }
+
+    const newAccessToken = generateAccessToken(user);
+    res.json({ token: newAccessToken });
+  });
+});
+
+
+userRouter.post('/login', async (req, res) => {
+  const { name, password } = req.body;
+
+  try {
+    const result = await pool.query(
+      'SELECT * FROM users WHERE name = $1',
+      [name]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const user = result.rows[0];
+
+  
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+   
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: false, 
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000 
+    });
+    
+    res.json({ token: accessToken });
+
+  } catch (err) {
+    console.error("Login error:", err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+userRouter.post("/register", async (req, res) => {
+  const { name, password } = req.body;
+
+  try {
+    const result = await userService.createUser(name, password);
+
+    if (result.error) {
+      return res.status(409).json(result);
+    }
+
+    return res.status(201).json(result);
+
+  } catch (err) {
+    console.error("Registration error:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
+
+
+userRouter.use(authenticateAccessToken);
+
+userRouter.get("/",async (req,res)=>{
     try{
-        const result = await pool.query(
-            "SELECT * FROM users"
-        );
-        res.status(200).json(result.rows);
+        const result = await userService.getUsers();
+        res.status(200).json(result);
     }catch(err){
         throw new Error(`Error ${err}`);
     }
 });
 
-userRouter.post("/users", async (req, res) => {
-  const { name, surname } = req.body;
-  try {
-    const checker = await pool.query(
-        `SELECT * FROM users WHERE name =$1 AND surname =$2`,[name,surname]
-    )
-    if(checker.rowCount > 0){
-        return res.status(409).json({error: "User already exist"})
-    }
-    const result = await pool.query(
-      "INSERT INTO users (name, surname) VALUES ($1, $2) RETURNING *",
-      [name, surname]
-    );
-    res.status(201).json(result.rows[0]);
-  } catch (err) {
-    console.error("DB error:", err);
-    res.status(500).json({ error: "Something went wrong" });
-  }
-});
 
-
-
-userRouter.put("/users/:id", async (req, res) => {
+userRouter.put("/:id", async (req, res) => {
   const { id } = req.params;
-  const { name, surname } = req.body;
-
+  const { name, password } = req.body;
   try {
-    const result = await pool.query(
-      `UPDATE users SET name = $1, surname = $2 WHERE id = $3 RETURNING *`,
-      [name, surname, id]
-    );
+    const updatedUser = await userService.updateUser(id, name, password);
 
-    if (result.rowCount === 0) {
+    if (!updatedUser) {
       return res.status(404).json({ error: "User not found" });
     }
 
-    res.status(200).json( {message : `User with ID ${id} updated successfully`});
+    res.status(200).json(updatedUser);
   } catch (err) {
-    console.error("Update error:", err);
+    console.error("Error updating user:", err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
 
-userRouter.delete("/users/:id", async (req, res) => {
+
+userRouter.delete("/:id", async (req, res) => {
   const { id } = req.params;
-
   try {
-    const result = await pool.query(
-      `DELETE FROM users WHERE id = $1`,
-      [id]
-    );
-
-    if (result.rowCount === 0) {
-      return res.status(404).json({ error: "User not found" });
+    const result = await userService.deleteUser(id);
+    if (result.error) {
+      return res.status(404).json(result);
     }
-
-    res.status(200).json({ message: "User deleted successfully" });
+    res.status(200).json(result);
   } catch (err) {
-    console.error("Delete error:", err);
+    console.error("Error deleting user:", err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
 
 export default userRouter;
---
+++ services/user.js
@@ -0,0 +1,84 @@
+
+  import pool from "../db.js"; 
+  import bcrypt from "bcrypt";
+
+  export default class UserService {
+    async createUser(name, password) {
+      try {
+        const userExists = await pool.query(
+          `SELECT * FROM users WHERE name = $1`,
+          [name]
+        );
+
+        if (userExists.rowCount > 0) {
+          console.log("User already exists:", name);
+          return { error: "User already exists" };
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const result = await pool.query(
+          "INSERT INTO users (name, password) VALUES ($1, $2) RETURNING *",
+          [name, hashedPassword]
+        );
+
+        console.log("User registered successfully:", result.rows[0]);
+        return result.rows[0];
+
+      } catch (err) {
+        console.error("DB error:", err);
+        throw new Error("Something went wrong");
+      }
+    }
+
+    async getUsers(){
+      try{
+          const result = await pool.query(
+              "SELECT * FROM users"
+          );
+          return result.rows;
+      }catch (err) {
+          console.error("DB error:", err);
+          throw new Error("Something went wrong");
+      }
+    }
+
+    async updateUser(id, name, password) {
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const result = await pool.query(
+        `UPDATE users SET name = $1, password = $2 WHERE id = $3 RETURNING *`,
+        [name, hashedPassword, id]
+      );
+
+      if (result.rowCount === 0) {
+        return null; 
+      }
+
+      return result.rows[0];
+    } catch (err) {
+      console.error("Update error:", err);
+      throw err;
+    }
+  }
+
+  async deleteUser(id){
+    try {
+      const result = await pool.query(
+        `DELETE FROM users WHERE id = $1 RETURNING name`,
+        [id]
+      );
+
+      if (result.rowCount === 0) {
+        return { error: "User not found" };
+      }
+
+      return { status: "User deleted successfully", user: result.rows[0] };
+    } catch (err) {
+      console.error("Delete error:", err);
+      throw err;
+    }
+  }
+
+  }
+
+  export const userService = new UserService();
